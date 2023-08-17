a80954d99902dc6ce5a59e5615460c31904e1e9b
ragi96 <rafael.giezendanner@frontify.com>
2023-08-17 19:45:55 +0200

    add keytakeaways below the selected text

--- packages/ai-content-block/src/hooks/useKeytakeaways.ts
+++ packages/ai-content-block/src/hooks/useKeytakeaways.ts
@@ -4,6 +4,7 @@ import { PlateEditor, getSelectionText } from '@udecode/plate';
 import { enhancedTextKeyTakeawaysExecutor } from '../GraphQL';
 import { useState } from 'react';
 import { insertContent } from '../helper';
+import { BaseEditor, Transforms } from 'slate';
 
 export const useKeytakeaways = () => {
     const [isKeytakeawaysLoading, setIsKeytakeawaysLoading] = useState(false);
@@ -12,6 +13,16 @@ export const useKeytakeaways = () => {
         setIsKeytakeawaysLoading(true);
         const response = await enhancedTextKeyTakeawaysExecutor({ text: value });
         if (response.enhancedText.keyTakeaways) {
+            if (editor.selection) {
+                const endpoint =
+                    editor.selection.focus.offset > editor.selection.anchor.offset
+                        ? editor.selection.focus
+                        : editor.selection.anchor;
+                Transforms.select(editor as BaseEditor, {
+                    anchor: endpoint,
+                    focus: endpoint,
+                });
+            }
             insertContent(editor, response.enhancedText.keyTakeaways);
         }
         setIsKeytakeawaysLoading(false);
